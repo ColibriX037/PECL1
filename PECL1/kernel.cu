67b3eb719a3d5fa6f9e9f3833cdb6f40887b0660
vivacasillas.98@gmail.com <5vrsh5f4kcayl4ia4solwrx2oupmubdny72gjahqwmfdhqhdxk5a>
2019-03-09 10:58:57 +0100

    Puesta a punto para el trabajo

--- PECL1/kernel.cu
+++ PECL1/kernel.cu
@@ -3,9 +3,78 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void showMatriz(int matriz[], int anchura);
+int * generateMatriz();
+void generateSeeds(int matriz[]);
+bool checkFull(int matriz[], int tamano);
+
+cudaError_t cudaStatus;
+bool partida_enCurso = true;
+
+__global__ void mov_up(int * matriz[]) {
+
+}
+
+cudaError_t move_up(int * matriz[]) {
+
+}
+
+__global__ void mov_down(int * matriz[]) {
+
+}
+
+cudaError_t move_down(int * matriz[]) {
+
+}
+
+__global__ void mov_left(int * matriz[]) {
+
+}
+
+cudaError_t move_left(int * matriz[]) {
+
+}
+
+__global__ void mov_right(int * matriz[]) {
+
+}
+
+cudaError_t move_right(int * matriz[]) {
+
+}
 
 int main()
 {
+	const int ancho = 4;
+	const int alto = 4;
+	int matriz[ancho*alto] = { 0 };
+
+	while (partida_enCurso) 
+	{
+		char movimiento = 'p';
+		printf("Tablero:\n");
+		showMatriz(matriz, 4);
+		printf("¿Hacia donde quieres mover?(w/a/s/d): ");
+		cin >> movimiento;
+		switch (movimiento)
+		{
+		case 'w':
+		case 'a':
+		case 's':
+		case 'd':
+
+		default:
+			break;
+		}
+		system("CLS");
+	}
+
+	
+
     cudaStatus = cudaDeviceReset();
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaDeviceReset failed!");
